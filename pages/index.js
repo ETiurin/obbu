080f6c7c79af59cb4e86d0bf994725465cd9d0a9
ETiurin <117716801+ETiurin@users.noreply.github.com>
2023-09-24 03:40:50 +0300

    added_menu_768

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
 const burger = document.querySelectorAll('.header__burger-btn');
 const modalOverlay = document.querySelector('.burger__modal-overlay');
-const modals = document.querySelectorAll('.burger__modals');
 
 burger.forEach((el) => {
     el.addEventListener('click', (e) => {
         let path  = e.currentTarget.getAttribute('data-path');
-
-        modals.forEach((el) => {
-            el.classList.remove('burger__modal-overlay--visible');
-        });
 
         document.querySelector(`[data-target="${path}"]`).classList.add('burger__modal--visible');
         modalOverlay.classList.add('burger__modal-overlay--visible');
