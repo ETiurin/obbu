5d886334b610d19b59fbc983176863e147337037
ETiurin <117716801+ETiurin@users.noreply.github.com>
2023-11-14 01:26:56 +0300

    added_home_grid_loading

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const menuBtns = document.querySelectorAll('.main__panel-button');
+    const drops = document.querySelectorAll('.main__panel-dropdown');
+
     menuBtns.forEach(el => {
         el.addEventListener("click", (e) => {
             let currentBtn = e.currentTarget;
@@ -8,13 +10,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
             menuBtns.forEach(el => {
                 if (el !== currentBtn) {
-                    el.classList.remove('.main__panel-element_active');
+                    el.classList.remove('main__panel-element_active');
                 }
             });
 
-            drop.classList.toogle('main__panel-dropdown_active');
-            currentBtn.classList.toogle('.main__panel-element_active');
+            drops.forEach(el => {
+                if (el !== drop) {
+                    el.classList.remove('main__panel-dropdown_active');
+                }
+            });
 
+            drop.classList.toggle('main__panel-dropdown_active');
+            currentBtn.classList.toggle('main__panel-element_active');
         });
     });
+
+    document.addEventListener('click', (e) => {
+        if  (!e.target.closest('.main__panel')) {
+            menuBtns.forEach(el => {
+                el.classList.remove('main__panel-element_active');
+            });
+
+            drops.forEach(el => {
+                el.classList.remove('main__panel-dropdown_active');
+            });
+        }
+    })
 });
